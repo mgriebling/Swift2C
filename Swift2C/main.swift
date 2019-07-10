8a88ed98b30b8d2dcbbe9391dfc22cebaf178d54
Michael Griebling <michael.griebling@solinst.com>
2019-07-10 12:21:53 -0400

    Initial Commit

---
+++ Swift2C/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift2C
+//
+//  Created by Michael Griebling on 7/10/19.
+//  Copyright © 2019 Solinst Canada. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
